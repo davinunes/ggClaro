2760b0caea1c077b2ef7f8a4937654a7a9abffee
davinunes <davi.nunes@gmail.com>
2021-10-11 20:29:49 +0000

    Atualização

--- php/git.php
+++ php/git.php
@@ -1,4 +1,10 @@
 <?php
+
+	/**
+	
+		Atualiza de forma prática o site da Geslane com o Github
+	
+	**/
 	
 	$command = 'cd /var/www/html/ggClaro && git pull && git add . && git commit -m "Atualização" && git push origin main';
 	$sh = "echo yuk11nn4 | su -c '$command' -s /bin/bash ilunne";
