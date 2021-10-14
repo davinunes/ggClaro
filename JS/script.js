e989cb3f6568740acc0389cedc9e45cad21d57ec
Davi Nunes <davi.nunes@gmail.com>
2021-10-14 13:53:16 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -187,7 +187,7 @@ $(document).on('keyup', '.somenteNumeros', function(){
 	$(this).val(this.value.replace(/\D/g, ''));
 });
 
-$(document).on('keyup', '.decimal', function(){
+$(document).on('blur', '.decimal', function(){
 	$(this).val(this.value.replace(',','.'));
 	//this.value = parseFloat(this.value).toFixed(2);
 });
